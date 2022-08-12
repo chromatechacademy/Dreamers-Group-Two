6b924163b865ab98adf5b95afc85cc15d9380773
GithubMagicwife <eligve@gmail.com>
2022-08-12 18:20:38 -0400

    Invalid Credentials Login Attempt

---
+++ src/test/java/com/stepDefinitions/InvalidCredentialsLoginStepDef.java
@@ -0,0 +1,20 @@
+package com.stepDefinitions;
+
+public class InvalidCredentialsLoginStepDef {
+
+    @Given("a user is on the chromatech website")
+    public void a_user_is_on_the_chromatech_website() {
+        
+    }
+
+    @When("a user enters invaalid credentials such as username {string} and password {string}")
+    public void a_user_enters_invaalid_credentials_such_as_username_and_password(String string, String string2) {
+        
+    }
+
+    @Then("a user should see message {string}")
+    public void a_user_should_see_message(String string) {
+        
+    }
+
+}
